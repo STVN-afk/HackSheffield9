6ad43e40b50c546937a1f97d224e0ccee7085901
lester <lesterlester2510x@gmail.com>
2024-11-16 12:42:51 +0000

    Made tree loop

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
 print("Hello World")
+
+treeLength = 500
+for i in range(treeLength):
+    if (i < (10/100)*treeLength):
+        print("leaf")
+    elif (i > (90/100)*treeLength):
+        print("stump")
+    else:
+        print("neck")
