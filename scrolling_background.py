bb522e8f6a2d9d9dd0a7bb7c2fe9cbe9db4dbe3c
Steve <negativedegre3s@gmail.com>
2024-11-16 15:50:12 +0000

    Added start screen

--- scrolling_background.py
+++ scrolling_background.py
@@ -33,6 +33,33 @@ DISPLAYSURF = pygame.display.set_mode((400, 600))
 DISPLAYSURF.fill(WHITE)
 pygame.display.set_caption("Game")
 
+# Start Screen
+def start_screen():
+    while True:
+        for event in pygame.event.get():
+            if event.type == QUIT:
+                pygame.quit()
+                sys.exit()
+            if event.type == MOUSEBUTTONDOWN:
+                if event.button == 1:  # Left mouse button click
+                    return  # Start the game loop
+
+        DISPLAYSURF.fill(WHITE)  # Fill the screen with white
+        title_text = font.render("Like a Sapling!", True, GREEN)
+        instructions_text = font_small.render("Click to Start", True, BLACK)
+
+        title_x = (SCREEN_WIDTH - title_text.get_width()) // 2
+        title_y = SCREEN_HEIGHT // 5  # Top part of the screen
+        DISPLAYSURF.blit(title_text, (title_x, title_y))
+
+        instructions_x = (SCREEN_WIDTH - instructions_text.get_width()) // 2
+        instructions_y = SCREEN_HEIGHT // 2  # Center of the screen
+        DISPLAYSURF.blit(instructions_text, (instructions_x, instructions_y))
+
+
+    pygame.display.update()  # Update the screen
+
+
 
 class Enemy(pygame.sprite.Sprite):
     def __init__(self):
@@ -110,78 +137,92 @@ class Background():
         DISPLAYSURF.blit(self.bgimage, (self.bgX2, self.bgY2))
 
 
+<<<<<<< Updated upstream
 
 
+=======
+def GameLoop():
+    global SCORE, SPEED
+>>>>>>> Stashed changes
 # Setting up Sprites
-P1 = Player()
-E1 = Enemy()
+    P1 = Player()
+    E1 = Enemy()
 
-back_ground = Background()
+    back_ground = Background()
 
 # Creating Sprites Groups
-enemies = pygame.sprite.Group()
-enemies.add(E1)
-all_sprites = pygame.sprite.Group()
-all_sprites.add(P1)
-all_sprites.add(E1)
+    enemies = pygame.sprite.Group()
+    enemies.add(E1)
+    all_sprites = pygame.sprite.Group()
+    all_sprites.add(P1)
+    all_sprites.add(E1)
 
 # Adding a new User event
-INC_SPEED = pygame.USEREVENT + 1
-pygame.time.set_timer(INC_SPEED, 1000)
+    INC_SPEED = pygame.USEREVENT + 1
+    pygame.time.set_timer(INC_SPEED, 1000)
 
-score = 0;
-font = pygame.font.SysFont(None, 36);
+    score = 0;
+    font = pygame.font.SysFont(None, 36);
 
-def spawn_enemy():
-    new_enemy = Enemy()
-    enemies.add(new_enemy)
-    all_sprites.add(new_enemy)
+    def spawn_enemy():
+        new_enemy = Enemy()
+        enemies.add(new_enemy)
+        all_sprites.add(new_enemy)
 
-def show_score():
-    score_surface = font.render("Score: " + str(score), True, BLACK)
-    DISPLAYSURF.blit(score_surface, (10, 10))
+    def show_score():
+        score_surface = font.render("Score: " + str(score), True, BLACK)
+        DISPLAYSURF.blit(score_surface, (10, 10))
 
-# Game Loop
-while True:
+    # Game Loop
+    while True:
 
     # Cycles through all occurring events
-    for event in pygame.event.get():
-        if event.type == INC_SPEED:
-            SPEED += 0.0
-        if event.type == QUIT:
-            pygame.quit()
-            sys.exit()
+        for event in pygame.event.get():
+            if event.type == INC_SPEED:
+                SPEED += 0.0
+            if event.type == QUIT:
+                pygame.quit()
+                sys.exit()
 
 
-    back_ground.update()
-    back_ground.render()
+        back_ground.update()
+        back_ground.render()
 
-    # DISPLAYSURF.blit(background, (0,0))
-    scores = font_small.render(str(SCORE), True, BLACK)
-    DISPLAYSURF.blit(scores, (10, 10))
+        # DISPLAYSURF.blit(background, (0,0))
+        scores = font_small.render(str(SCORE), True, BLACK)
+        DISPLAYSURF.blit(scores, (10, 10))
 
     # Moves and Re-draws all Sprites
-    for entity in all_sprites:
-        DISPLAYSURF.blit(entity.image, entity.rect)
-        entity.move()
+        for entity in all_sprites:
+            DISPLAYSURF.blit(entity.image, entity.rect)
+            entity.move()
 
+<<<<<<< Updated upstream
+=======
+
+        P1.update()
+
+>>>>>>> Stashed changes
     # To be run if collision occurs between Player and Enemy
-    if pygame.sprite.spritecollideany(P1, enemies):
+        if pygame.sprite.spritecollideany(P1, enemies):
 
         #increase player score
-        score+=10
+            score+=10
 
-        collided_enemy= pygame.sprite.spritecollideany(P1, enemies)
-        if collided_enemy:
-            collided_enemy.kill()
-            spawn_enemy()  # Spawn a new enemy immediately after collision
+            collided_enemy= pygame.sprite.spritecollideany(P1, enemies)
+            if collided_enemy:
+                collided_enemy.kill()
+                spawn_enemy()  # Spawn a new enemy immediately after collision
 
         if (P1.width < 300 and P1.height < 500):
             P1.resize(scale_factor=1.05)
         #refresh screen
+            show_score()
+            pygame.display.update()
+
         show_score()
         pygame.display.update()
+        FramePerSec.tick(FPS)
 
-    show_score()
-    pygame.display.update()
-    FramePerSec.tick(FPS)
+start_screen()
+GameLoop()
