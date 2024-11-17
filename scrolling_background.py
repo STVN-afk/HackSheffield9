494ad08043ccf0c0d15aedaeba57567342ab49fb
lester <lesterlester2510x@gmail.com>
2024-11-17 11:08:25 +0000

    Made it so that the background music plays when in the game

--- scrolling_background.py
+++ scrolling_background.py
@@ -36,15 +36,14 @@ pygame.display.set_caption("Like a Sapling!")
 
 pygame.mouse.set_visible(False)
 
-pygame.mixer.music.load("StartMusic.mp3")
-pygame.mixer.music.set_volume(0.5)  # Set volume (0.0 to 1.0)
-pygame.mixer.music.play()  # Loop the music indefinitely
-
 click_sound = pygame.mixer.Sound("Click.mp3")  # Replace with your sound effect file
 click_sound.set_volume(1.0)  # Set volume (0.0 to 1.0)
 
 # Start Screen
 def start_screen():
+    pygame.mixer.music.load("StartMusic.mp3")
+    pygame.mixer.music.set_volume(0.5)  # Set volume (0.0 to 1.0)
+    pygame.mixer.music.play()  # Loop the music indefinitely
     while True:
         for event in pygame.event.get():
             if event.type == QUIT:
@@ -257,6 +256,9 @@ class Background():
 
 
 def GameLoop():
+    pygame.mixer.music.load("Attack on Titan ⧸ Shingeki no Kyojin Opening - Guren no Yumiya 8-bit and 16-bit Remix.mp3")
+    pygame.mixer.music.set_volume(0.5)  # Set volume (0.0 to 1.0)
+    pygame.mixer.music.play()  # Loop the music indefinitely
     global SCORE, SPEED, SPAWN_TIMER
 
     spawn_interval = 1000  # Start with 1000 ms (1 second)
